f2d061f47f67819546897b268663bd84b1b93645
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2023-12-30 16:38:18 +0400

    Add file via git

--- extension.js
+++ extension.js
@@ -5,32 +5,147 @@ const vscode = require('vscode');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
+const CONFIG_KEY_NAME = 'chatpilot.greetingName';
+const CONFIG_KEY_CHECKBOX = 'chatpilot.showWelcomeCheckbox';
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	console.log("activate")
+    updateCommentSymbol(); // Appel initial pour le fichier actif au démarrage de l'extension
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "chatpilot" is now active!');
+    let userName = vscode.workspace.getConfiguration().get(CONFIG_KEY_NAME, '');
+    let showWelcomeCheckbox = vscode.workspace.getConfiguration().get(CONFIG_KEY_CHECKBOX, true);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chatpilot.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+    if (!userName && showWelcomeCheckbox) {
+		console.log("first start")
+        vscode.window.showInformationMessage(
+            'Bienvenue dans Visual Studio Code !',
+            'Configurer',
+            'Ne plus afficher'
+        ).then((selected) => {
+            if (selected === 'Configurer') {
+                showConfigInput();
+            } else if (selected === 'Ne plus afficher') {
+                vscode.workspace.getConfiguration().update(CONFIG_KEY_CHECKBOX, false, vscode.ConfigurationTarget.Global);
+            }
+        });
+    } else {
+		console.log("already start")
+        showWelcomeMessage(userName);
+    }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ChatPilot!');
-	});
-
-	context.subscriptions.push(disposable);
+    // Écouter les changements dans l'éditeur
+    vscode.window.onDidChangeActiveTextEditor(() => {
+        updateCommentSymbol(); // Appeler la fonction à chaque changement d'éditeur actif
+    });
 }
 
-// This method is called when your extension is deactivated
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+function updateCommentSymbol(context) {
+    let activeTextEditor = vscode.window.activeTextEditor;
+    if (!activeTextEditor) {
+		console.log("pas de editor")
+        return;
+    }
+	console.log("ok editor")
+
+    let document = activeTextEditor.document;
+    let languageId = document.languageId;
+	// console.log(document.lineAt(1).text)
+    let commentSymbol = getCommentSymbol(languageId);
+	// console.log(languageId)
+	// console.log(commentSymbol)
+
+    if (commentSymbol) {
+        let commentLines = getCommentOnlyLines(document, commentSymbol);
+        // console.log(`Lignes avec uniquement des commentaires : \n${commentLines.map(line => line.line).join('\n')}`);
+
+        // Effacer les décorations précédentes
+        clearDecorations();
+		var decorations = []
+
+        // Ajouter des décorations pour chaque ligne avec un bouton
+        commentLines.forEach((lineInfo) => {
+            let lineNumber = lineInfo.lineNumber;
+            // let line = lineInfo.line;
+            let decoration = createDecoration(lineNumber);
+            decorations.push(decoration)
+
+            // Ajouter un gestionnaire d'événements pour le clic sur la décoration
+            // decoration.command = {
+            //     command: 'extension.showComment',
+            //     title: 'Afficher le commentaire',
+            //     arguments: [line]
+            // };
+            // context.subscriptions.push(decoration.command);
+        });
+		activeTextEditor.setDecorations(decorationType, decorations);
+    }
+}
+
+function getCommentOnlyLines(document, commentSymbol) {
+    let commentLines = [];
+    for (let i = 0; i < document.lineCount; i++) {
+        let line = document.lineAt(i).text.trim();
+        if (line.startsWith(commentSymbol)) {
+            commentLines.push({ line: line, lineNumber: i + 1 });
+        }
+    }
+    return commentLines;
+}
+
+function createDecoration(lineNumber) {
+    let range = new vscode.Range(lineNumber - 1, 0, lineNumber - 1, 0);
+    let decoration = { range: range, renderOptions: { after: { contentText: '🟢', color: 'green' } } };
+    return decoration;
+}
+
+function clearDecorations() {
+    let activeTextEditor = vscode.window.activeTextEditor;
+    if (activeTextEditor) {
+        activeTextEditor.setDecorations(decorationType, []);
+    }
+}
+
+function showConfigInput() {
+    vscode.window.showInputBox({
+        prompt: 'Entrez votre nom',
+        placeHolder: 'Nom'
+    }).then((inputName) => {
+        if (inputName) {
+            vscode.workspace.getConfiguration().update(CONFIG_KEY_NAME, inputName, vscode.ConfigurationTarget.Global).then(() => {
+                showWelcomeMessage(inputName);
+            });
+        }
+    });
+}
+
+function showWelcomeMessage(userName) {
+    vscode.window.showInformationMessage(`Bonjour ${userName} ! Bienvenue dans Visual Studio Code.`);
+}
+
+function getCommentSymbol(languageId) {
+	
+    if (languageId == "python") {
+		return '#'
+	} else return ""
+}
+
 function deactivate() {}
 
+const decorationType = vscode.window.createTextEditorDecorationType({});
+
+// Ajouter une commande pour gérer le clic sur la décoration
+vscode.commands.registerCommand('extension.showComment', (line) => {
+    console.log(`Contenu du commentaire : ${line}`);
+});
+
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
---
+++ test.js
@@ -0,0 +1,69 @@
+const vscode = require('vscode');
+
+let panel;
+
+function activate(context) {
+    vscode.window.onDidChangeActiveTextEditor(updatePanelContent);
+    vscode.workspace.onDidChangeTextDocument(updatePanelContent);
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.showSidebar', () => {
+            if (!panel) {
+                // Créer une nouvelle vue dans la barre latérale
+                panel = vscode.window.createWebviewPanel(
+                    'sidebarSample',
+                    'Sidebar Sample',
+                    vscode.ViewColumn.One,
+                    {
+                        enableScripts: true
+                    }
+                );
+
+                // Mettre à jour le contenu initial
+                updatePanelContent();
+            }
+        })
+    );
+}
+
+function updatePanelContent() {
+    if (panel && panel.visible) {
+        const activeEditor = vscode.window.activeTextEditor;
+
+        let content = 'Aucun éditeur actif.';
+        if (activeEditor) {
+            const lineCount = activeEditor.document.lineCount;
+            content = `Nombre de lignes dans le fichier actif : ${lineCount}`;
+        }
+
+        // Mettre à jour le contenu de la vue
+        panel.webview.html = getWebviewContent(content);
+    }
+}
+
+function getWebviewContent(content) {
+    return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Sidebar Sample</title>
+        </head>
+        <body>
+            <h2>Informations</h2>
+            <p>${content}</p>
+            <button onclick="alert('Bouton cliqué !')">Cliquez-moi</button>
+        </body>
+        </html>`;
+}
+
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+    if (panel) {
+        panel.dispose();
+    }
+}
+
+exports.deactivate = deactivate;
